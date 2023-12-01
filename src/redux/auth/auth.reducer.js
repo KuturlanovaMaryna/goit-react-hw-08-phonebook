ab59d6a2c6888c14601bec8eef938b4dd64dd534
KuturlanovaMaryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2023-12-01 15:48:30 +0200

    fix

--- src/redux/auth/auth.reducer.js
+++ src/redux/auth/auth.reducer.js
@@ -1,43 +1,43 @@
-import { createSlice, createAsyncThunk,isAnyOf } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice} from "@reduxjs/toolkit";
+// import axios from "axios";
 
-export const fetchAllContacts = createAsyncThunk(
-  'contacts/getContacts',
-  async (_, thunkApi) => {
-    try {
-      const { data } = await axios.get(`https://655e44c19f1e1093c59ad4be.mockapi.io/contacts`)
-      console.log('data:',data)
-      return data
-    } catch (err) {
+// export const fetchAllContacts = createAsyncThunk(
+//   'contacts/getContacts',
+//   async (_, thunkApi) => {
+//     try {
+//       const { data } = await axios.get(`https://655e44c19f1e1093c59ad4be.mockapi.io/contacts`)
+//       console.log('data:',data)
+//       return data
+//     } catch (err) {
      
-      return thunkApi.rejectWithValue(err.message)
-    }
-  }
-)
+//       return thunkApi.rejectWithValue(err.message)
+//     }
+//   }
+// )
 
-export const fetchAddContact = createAsyncThunk(
-  'contacts/addContact',
-  async (contact, thunkApi) => {
-    try {
-      const { data } = await axios.post(`https://655e44c19f1e1093c59ad4be.mockapi.io/contacts/`, contact);
-      return data;
-    } catch (err) {
-      return thunkApi.rejectWithValue(err.message);
-    }
-  }
-);
+// export const fetchAddContact = createAsyncThunk(
+//   'contacts/addContact',
+//   async (contact, thunkApi) => {
+//     try {
+//       const { data } = await axios.post(`https://655e44c19f1e1093c59ad4be.mockapi.io/contacts/`, contact);
+//       return data;
+//     } catch (err) {
+//       return thunkApi.rejectWithValue(err.message);
+//     }
+//   }
+// );
 
-export const fetchDeleteContact = createAsyncThunk(
-  'contacts/deleteContact',
-  async (contactId, thunkApi) => {
-    try {
-      const { data } = await axios.delete(`https://655e44c19f1e1093c59ad4be.mockapi.io/contacts/${contactId}`);
-      return data;
-    } catch (err) {
-      return thunkApi.rejectWithValue(err.message);
-    }
-  }
-)
+// export const fetchDeleteContact = createAsyncThunk(
+//   'contacts/deleteContact',
+//   async (contactId, thunkApi) => {
+//     try {
+//       const { data } = await axios.delete(`https://655e44c19f1e1093c59ad4be.mockapi.io/contacts/${contactId}`);
+//       return data;
+//     } catch (err) {
+//       return thunkApi.rejectWithValue(err.message);
+//     }
+//   }
+// )
 
 const initialState = {
     isAuth:false,
