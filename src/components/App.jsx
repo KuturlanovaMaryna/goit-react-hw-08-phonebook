0f065e32287b14a1ad022d289576a4fba72eeedb
KuturlanovaMaryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2023-12-04 16:10:22 +0200

    update private routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,12 @@ import css from './App.module.css';
 import { Layout } from './Layout/Layout';
 import { useDispatch } from 'react-redux';
 import { refreshThunk } from '../redux/auth/auth.reducer';
+import { Suspense } from 'react';
+import Loader from './Loader/Loader';
+
+import * as ROUTES from '../constants/routs';
+import RestrictedRoute from './RestrictedRoute';
+import PrivateRoute from './PrivateRote';
 
 const Home = lazy(() => import('pages/HomePage'));
 const Registrated = lazy(() => import('pages/RegistratedPage'));
@@ -19,15 +25,48 @@ const App = () => {
 
   return (
     <div className={css.appContainer}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/register" element={<Registrated />} />
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/contacts" element={<Contacts />} />
-        </Route>
-        <Route path="*" element={<Home />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route
+              path={ROUTES.HOME_ROUTE}
+              element={
+                <RestrictedRoute>
+                  <Home />
+                </RestrictedRoute>
+              }
+            ></Route>
+
+            <Route
+              path={ROUTES.CONTACTS_ROUTE}
+              element={
+                <PrivateRoute>
+                  <Contacts />
+                </PrivateRoute>
+              }
+            ></Route>
+
+            <Route
+              path={ROUTES.LOGIN_ROUTE}
+              element={
+                <RestrictedRoute>
+                  <LogIn />
+                </RestrictedRoute>
+              }
+            ></Route>
+
+            <Route
+              path={ROUTES.REGISTER_ROUTE}
+              element={
+                <RestrictedRoute>
+                  <Registrated />
+                </RestrictedRoute>
+              }
+            ></Route>
+          </Route>
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
